0be8a93b006d4398ca2f6caf0a05fdae9717020d
unknown <cruchyn@gmail.com>
2026-05-20 21:24:12 +0700

    Scheduler + Soft Delete file & Scheduled Job

--- routes/console.php
+++ routes/console.php
@@ -1,7 +1,15 @@
 <?php
 
+
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
+
+// Jalankan pengecekan janji temu setiap hari jam 07:00 pagi
+Schedule::command('hospital:process-appointments')->dailyAt('07:00');
+
+// Jalankan pembersihan file sampah setiap hari tengah malam
+Schedule::command('hospital:clear-deleted-files')->daily();
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
